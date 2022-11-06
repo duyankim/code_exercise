ed3ba83a5b3858d4a750672062392e70fac3843b
Duyan Kim <arbolkim@gmail.com>
2022-11-06 16:27:39 +0900

    feat: [자료구조1_01] 백준 9012번 괄호 문제 `132ms`

---
+++ src/data_structure_1/BJ9012.java
@@ -0,0 +1,48 @@
+package data_structure_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ9012 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int lines = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < lines; i++) {
+            st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();
+
+            if (str.length()%2 != 0) {
+                System.out.println("NO");
+                continue;
+            } else if (str.charAt(0) != '(' || str.charAt(str.length()-1) != ')') {
+                System.out.println("NO");
+                continue;
+            } else {
+                int cnt = 0;
+
+                for (int j = 0; j < str.length(); j++) {
+
+                    if (str.charAt(j) == '(') {
+                        cnt++;
+                    } else {
+                        cnt--;
+                    }
+
+                    if (cnt < 0) {
+                        System.out.println("NO");
+                        break;
+                    } else if (j == str.length() - 1 && cnt == 0) {
+                        System.out.println("YES");
+                    } else if (j == str.length() - 1 && cnt != 0) {
+                        System.out.println("NO");
+                    }
+                }
+            }
+        }
+    }
+}

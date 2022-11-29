606734733a8da6e5ece89853691664801e74e23e
Duyan Kim <arbolkim@gmail.com>
2022-11-29 23:14:50 +0900

    feat: [자료구조1_08] 백준 2346번 풍선터뜨리기 문제 `180ms`

---
+++ src/data_structure_1/BJ2346.java
@@ -0,0 +1,49 @@
+package data_structure_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class BJ2346 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int elements = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+
+        Deque<int[]> ballons = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
+        sb.append("1 ");
+
+        int idx = 0;
+        int nxtMove = Integer.parseInt(st.nextToken());
+
+        for (int i = 2; i <= elements; i++) {
+            ballons.add(new int[]{i, Integer.parseInt(st.nextToken())});
+        }
+
+        while (!ballons.isEmpty()) {
+            int[] out;
+            if (nxtMove > 0) {
+                for (int i = 1; i < nxtMove; i++) {
+                    ballons.add(ballons.pollFirst());
+                }
+                out = ballons.removeFirst();
+            } else {
+                for (int i = nxtMove; i < -1; i++) {
+                    ballons.addFirst(ballons.pollLast());
+                }
+                out = ballons.removeLast();
+            }
+            nxtMove = out[1];
+            sb.append(out[0]).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
